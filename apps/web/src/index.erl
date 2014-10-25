18c95809f4c32c810bc19df4670fb63bd12d7b92
Evgeny Tataurov <tatauroff@gmail.com>
2014-10-25 22:24:55 +0600

    some styles, cleaner code

--- apps/web/src/index.erl
+++ apps/web/src/index.erl
@@ -5,11 +5,9 @@
 main() ->
 	#dtl{file="index", bindings=[{body, body()}]}.
 body() -> 
-    [#panel{body=[
-      #radiogroup { id=radioGroup, body=[
-            #radio { id=catsRadio, body="#cats", value = <<"cats">>, postback={tag, <<"cats">>}, checked=true }, #br{},
-            #radio { id=goodRadio, body="#instagood", value = <<"instagood">>, postback={tag, <<"instagood">>} }, #br{}
-    ]}
+    [#panel{id=butPanel, body=[
+       #button{id=catsButton, class= <<"btn btn-primary btn-lg btn-block">>, 
+               body= <<"show #cats">>, postback={tag, <<"cats">>}}
     ]},
 	#panel{id=updater}].
 
@@ -17,16 +15,11 @@ event(init) ->
     ok;
 
 event(terminate) ->
-    get(listener) ! stop,
-    erase(listener);
+    ok;
 
 event({tag, Tag}) ->
-    case get(listener) of
-        undefined -> pass;
-        Process -> Process ! stop
-    end,
-    {ok, Pid} = wf:async(random:uniform(100000), fun() -> loop_start(updater, Tag) end),
-    put(listener, Pid),
+    wf:update(butPanel, #h1{body= << <<"#">>/binary, Tag/binary>>}),
+    {ok, _Pid} = wf:async(Tag, fun() -> loop_start(updater, Tag) end),
     wf:reg(Tag).
 
 loop_start(E, Tag) ->
@@ -34,25 +27,31 @@ loop_start(E, Tag) ->
     manager:subscribe(Tag),
 	loop(E, Tag).
 
+insert_image(Elem, Img) ->
+    wf:insert_top(Elem,
+                   #panel{class= <<"row">>, body=#panel{class= <<"col-md-4 col-md-offset-4">>, body=[
+                    #link{href=maps:get(<<"link">>, Img),
+                          body=#image{src=maps:get(<<"url">>,
+                                        maps:get(<<"low_resolution">>,
+                                        maps:get(<<"images">>, Img))),
+                                     class= <<"img-thumbnail">>}},
+                    #panel{body=wf:f("by ~s", [maps:get(<<"username">>, maps:get(<<"user">>, Img))])}]}}).
+
+
 loop(Elem, Tag) ->
-	receive
+    receive
         {gproc_ps_event, {tag, Tag}, Text} ->
            case maps:get(<<"type">>, Text) of
                <<"image">> ->
-                  wf:insert_top(Elem,
-                                   #panel{body=[
-                                    #link{href=maps:get(<<"link">>, Text),
-                                          body=#image{src=maps:get(<<"url">>,
-                                                                   maps:get(<<"low_resolution">>,
-                                                                            maps:get(<<"images">>, Text)))}},
-                                    #panel{body=wf:f("by ~s", [maps:get(<<"username">>, maps:get(<<"user">>, Text))])}]}),
+                   insert_image(Elem, Text),
                    wf:flush(Tag);
                 _ ->
                    ok
            end,
+           timer:sleep(500),
            gproc_ps:enable_single(l, {tag, Tag}),
 	       loop(Elem, Tag);
-		stop ->
+		_ ->
             gproc_ps:delete_single(l, {tag, Tag}),
-			ok
+			stop
 	end.
